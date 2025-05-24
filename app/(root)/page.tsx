abfffc11f4a1c86a108e661e6dd07cbf011d789f
GotLib567 <gotlibweb@mail.ru>
2025-05-24 23:18:20 +0500

    home layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+
+const Home = () => (
+  <>
+    <BookOverview />
+
+    <BookList />
+  </>
+)
+
+export default Home;
